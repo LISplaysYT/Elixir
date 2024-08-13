deede6f79b4925f7375a8f5dfd04213f26bb64d4
LISPlays <lisplaysyt@gmail.com>
2024-08-12 19:11:50 -0700

    S

---
+++ .history/static/public/js/sw_20240811111713.js
@@ -0,0 +1,21 @@
+var cacheName = 'loung3f2x';
+var filesToCache = [
+  '/js/sw.js'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
